1b94366b7c81a79a10727502bb1b65a773b0dd1e
road2Coder <djaffre.prog@gmail.com>
2023-07-07 19:56:44 +0200

    Modification

--- Projects/7. Pomodoro/app.js
+++ Projects/7. Pomodoro/app.js
@@ -1,3 +1,4 @@
+// Sélection des éléments HTML
 const displayWork = document.querySelector("#displayWork");
 const displayBreak = document.querySelector("#displayBreak");
 const progressWork = document.querySelector("#progressWork");
@@ -6,115 +7,130 @@ const btnPlay = document.querySelector("#btnPlay");
 const btnReset = document.querySelector("#btnReset");
 const displayCount = document.querySelector("#displayCount");
 
+// Initialisation des variables
 let minutesWork = 29;
-let minutesBreak = 4;
+let minutesBreak = 7;
 let secondesWork = 59;
 let secondesBreak = 59;
 
 let count = 0;
 let isPaused = true;
 
+// Appel de la fonction playEvent
 playEvent();
 
-// Remplacez les déclarations de intervalWork et intervalBreak par les suivantes
+// Déclaration des variables de timeout
 let timeoutWork;
 let timeoutBreak;
 
-// Dans les fonctions timerWork et timerBreak, remplacez setInterval par setTimeout
+// Fonction de compte à rebours pour la partie travail
 function timerWork() {
   if (secondesWork === 0 && minutesWork === 0) {
-    clearInterval(intervalWork);
-    progressWork.classList.add("hidden");
-    displayWork.classList.add("mt-1.5");
+    // Si le temps de travail est écoulé
+    toggleDisplays(progressWork, displayWork, true);
     displayWork.textContent = "0:00";
     timerBreak();
   } else {
-    progressWork.classList.remove("hidden");
-    displayWork.classList.remove("mt-1.5");
-    displayWork.textContent = `${minutesWork}:${String(secondesWork).padStart(2, "0")}`;
+    // Si le temps de travail n'est pas encore écoulé
+    toggleDisplays(progressWork, displayWork, false);
+    displayWork.textContent = `${minutesWork}:${String(secondesWork).padStart(
+      2,
+      "0"
+    )}`;
     secondesWork -= 1;
     if (secondesWork < 0) {
       secondesWork = 59;
       minutesWork--;
     }
-    timeoutWork = setTimeout(timerWork, 1000); // Utilisez setTimeout ici
+    timeoutWork = setTimeout(timerWork, 1000);
   }
 }
 
+// Fonction de compte à rebours pour la partie repos
 function timerBreak() {
-  if (secondesBreak === 0 && minutesBreak === 0 && secondesWork === 0 && minutesWork === 0) {
-    clearInterval(intervalBreak);
-    progressBreak.classList.add("hidden");
-    displayBreak.classList.add("mt-1.5");
+  if (
+    secondesBreak === 0 &&
+    minutesBreak === 0 &&
+    secondesWork === 0 &&
+    minutesWork === 0
+  ) {
+    // Si le temps de repos est écoulé
+    toggleDisplays(progressBreak, displayBreak, true);
     displayWork.textContent = "30:00";
     displayBreak.textContent = "5:00";
     count++;
     displayCount.innerHTML = `<span id="displayCount">&nbsp;${count}</span>`;
-    restetMinSec();
+    resetMinSec();
     playEvent();
     timerWork();
   } else {
-    progressWork.classList.add("hidden");
-    displayWork.classList.add("mt-1.5");
-    progressBreak.classList.remove("hidden");
-    displayBreak.classList.remove("mt-1.5");
-    displayBreak.textContent = `${minutesBreak}:${String(secondesBreak).padStart(2, "0")}`;
+    // Si le temps de repos n'est pas encore écoulé
+    toggleDisplays(progressWork, displayWork, true);
+    toggleDisplays(progressBreak, displayBreak, false);
+    displayBreak.textContent = `${minutesBreak}:${String(
+      secondesBreak
+    ).padStart(2, "0")}`;
     secondesBreak -= 1;
     if (secondesBreak < 0) {
       secondesBreak = 59;
       minutesBreak--;
     }
-    timeoutBreak = setTimeout(timerBreak, 1000); // Utilisez setTimeout ici
+    timeoutBreak = setTimeout(timerBreak, 1000);
   }
 }
 
-// Dans la fonction toggleTimer, ajoutez la logique pour annuler les timeouts
+// Fonction de basculement entre play et pause
 function toggleTimer() {
   if (isPaused) {
+    // Si en pause, passer en mode play
     isPaused = false;
-    btnPlay.innerHTML = '<div id="btnPlay" class="cursor-pointer border bg-zinc-100 rounded-md w-16 h-16 flex items-center justify-center shadow-xl"><img src="./ressources/pause.svg" width="35px" alt=""></div>';
+    btnPlay.innerHTML =
+      '<div id="btnPlay" class="cursor-pointer border bg-zinc-100 rounded-md w-16 h-16 flex items-center justify-center shadow-xl"><img src="./ressources/pause.svg" width="35px" alt=""></div>';
     timerWork();
   } else {
-    clearTimeout(timeoutWork); // Annule le timeout de timerWork
-    progressWork.classList.add("hidden");
-    displayWork.classList.add("mt-1.5");
-    clearTimeout(timeoutBreak); // Annule le timeout de timerBreak
-    progressBreak.classList.add("hidden");
-    displayBreak.classList.add("mt-1.5");
+    // Si en mode play, passer en pause
+    clearTimeout(timeoutWork);
+    toggleDisplays(progressWork, displayWork, true);
+    clearTimeout(timeoutBreak);
+    toggleDisplays(progressBreak, displayBreak, true);
     isPaused = true;
-    btnPlay.innerHTML = '<div id="btnPlay" class="cursor-pointer border bg-zinc-100 rounded-md w-16 h-16 flex items-center justify-center shadow-xl"><img src="./ressources/play.svg" width="35px" alt=""></div>';
+    btnPlay.innerHTML =
+      '<div id="btnPlay" class="cursor-pointer border bg-zinc-100 rounded-md w-16 h-16 flex items-center justify-center shadow-xl"><img src="./ressources/play.svg" width="35px" alt=""></div>';
   }
 }
 
-
-// Bouton reset
+// Gestionnaire d'événement pour le bouton Reset
 btnReset.addEventListener("click", () => {
-  clearInterval(timeoutWork);
+  clearTimeout(timeoutWork);
   clearTimeout(timeoutBreak);
-  progressWork.classList.add("hidden");
-  displayWork.classList.add("mt-1.5");
-  progressBreak.classList.add("hidden");
-  displayBreak.classList.add("mt-1.5");
-  restetMinSec();
+  toggleDisplays(progressWork, displayWork, true);
+  toggleDisplays(progressBreak, displayBreak, true);
+  resetMinSec();
   count = 0;
   isPaused = true;
-  
-  btnPlay.innerHTML = '<div id="btnPlay" class="cursor-pointer border bg-zinc-100 rounded-md w-16 h-16 flex items-center justify-center shadow-xl"><img src="./ressources/play.svg" width="35px" alt=""></div>';
+
+  btnPlay.innerHTML =
+    '<div id="btnPlay" class="cursor-pointer border bg-zinc-100 rounded-md w-16 h-16 flex items-center justify-center shadow-xl"><img src="./ressources/play.svg" width="35px" alt=""></div>';
   displayCount.innerHTML = `<span id="displayCount">&nbsp;${count}</span>`;
   displayWork.textContent = "30:00";
   displayBreak.textContent = "5:00";
 });
 
-// Fonction play
+// Fonction d'écoute de l'événement de lecture
 function playEvent() {
   btnPlay.addEventListener("click", toggleTimer);
 }
 
-
-// Reset des minutes et secondes
-function restetMinSec() {
+// Fonction de réinitialisation des minutes et secondes
+function resetMinSec() {
   minutesWork = 29;
   minutesBreak = 4;
-  secondesWork = 59;
-  secondesBreak = 9;
+  secondesWork =  59;
+  secondesBreak = 59;
+}
+
+// Fonction pour basculer l'affichage des éléments
+function toggleDisplays(progress, display, hidden) {
+  progress.classList.toggle("hidden", hidden);
+  display.classList.toggle("mt-1.5", hidden);
 }
